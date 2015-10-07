c51732934b205165e47d451b0d144c880a04d4c7
joaorpsoares <joao_rp_soares@hotmail.com>
2015-10-07 16:10:48 +0100

    Ilumination parsing

--- reader/MySceneGraph.js
+++ reader/MySceneGraph.js
@@ -48,6 +48,46 @@ MySceneGraph.prototype.parseInitials= function(rootElement) {
 
 }
 
+MySceneGraph.prototype.parseIlumination= function(rootElement) {
+
+	console.log("ILUMINATION: \n");
+
+	var ilumination = rootElement.getElementsByTagName('ILLUMINATION');
+	if(ilumination == null) return "ILLUMINATION tag not found!";
+
+
+	var iluminationInfo = ilumination[0];
+
+	var ambient = iluminationInfo.getElementsByTagName("ambient");
+	if(ambient == null) return "AMBIENT not found!";
+
+	console.log("AMBIENT: ");
+
+	var ambientInfo = ambient[0];
+	this.ambientInfo = [];
+	this.ambientInfo["r"] = this.reader.getFloat(ambientInfo, "r", true);
+	this.ambientInfo["g"] = this.reader.getFloat(ambientInfo, "g", true);
+	this.ambientInfo["b"] = this.reader.getFloat(ambientInfo, "b", true);
+	this.ambientInfo["a"] = this.reader.getFloat(ambientInfo, "a", true);
+
+	console.log("R: " + this.ambientInfo["r"] + "G: " + this.ambientInfo["g"] + "B: " + this.ambientInfo["g"] + "A: " + this.ambientInfo["a"] + "\n");
+
+	var background = iluminationInfo.getElementsByTagName("background");
+	if(background == null) return "BACKGROUND not found!";
+
+	console.log("BACKGROUND: ");
+
+	var backgroundInfo = background[0];
+	this.backgroundInfo = [];
+	this.backgroundInfo["r"] = this.reader.getFloat(backgroundInfo, "r", true);
+	this.backgroundInfo["g"] = this.reader.getFloat(backgroundInfo, "g", true);
+	this.backgroundInfo["b"] = this.reader.getFloat(backgroundInfo, "b", true);
+	this.backgroundInfo["a"] = this.reader.getFloat(backgroundInfo, "a", true);
+
+	console.log("R: " + this.backgroundInfo["r"] + "G: " + this.backgroundInfo["g"] + "B: " + this.backgroundInfo["g"] + "A: " + this.backgroundInfo["a"] + "\n");
+	
+}
+
 
 /*
  * Example of method that parses elements of one block and stores information in a specific data structure
