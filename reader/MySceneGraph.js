5da6897dcacf60d4cd6ea1a5be2671f66c1c15e7
alfa04 <joao.carlos.costa@fe.up.pt>
2015-10-07 17:19:45 +0100

    ParseInitials

--- reader/MySceneGraph.js
+++ reader/MySceneGraph.js
@@ -27,7 +27,7 @@ MySceneGraph.prototype.onXMLReady=function()
 	var rootElement = this.reader.xmlDoc.documentElement;
 	
 	// Here should go the calls for different functions to parse the various blocks
-	var error = this.parseGlobalsExample(rootElement);
+	var error = this.parseInitials(rootElement);
 
 	if (error != null) {
 		this.onXMLError(error);
@@ -40,11 +40,94 @@ MySceneGraph.prototype.onXMLReady=function()
 	this.scene.onGraphLoaded();
 };
 
+// Parser INITIALS
 MySceneGraph.prototype.parseInitials= function(rootElement) {
 
 	console.log("INITIALS: \n");
-
 	this.initialsInfo={};
+	var elems = rootElement.getElementsByTagName('INITIALS');
+
+	if(elems == null) return "'INITIALS' tag is missing.";
+	if(elems.length != 1) return "More than one 'INITIALS' tag.";
+
+	var initials = elems[0];
+
+	//Frustum
+	elemsFrustum = initials.getElementsByTagName('frustum');
+	if(elemsFrustum == null) return "'frustum' tag is missing.";
+	if(elemsFrustum.length != 1) return "More than one 'frustum' tag.";
+	var frustum = elemsFrustum[0];
+
+	this.initialsInfo.frustum={};
+
+	this.initialsInfo.frustum['near'] = this.reader.getFloat(frustum,'near',true);
+	this.initialsInfo.frustum['far'] = this.reader.getFloat(frustum,'far',true);
+
+	if(isNaN(this.initialsInfo.frustum['near'])) return "'frustum near' must be a float.";
+	if(isNaN(this.initialsInfo.frustum['far'])) return "'frustum far' must be a float";
+	
+	//Translation
+	elemsTranslation = initials.getElementsByTagName('translation');
+	if(elemsTranslation == null) return "'translation' tag is missing.";
+	if(elemsTranslation.length != 1) return "More than one 'translation' tag.";
+	var translation = elemsTranslation[0];
+
+	this.initialsInfo.translation={};
+
+	this.initialsInfo.translation['x'] = this.reader.getFloat(translation,'x',true);
+	this.initialsInfo.translation['y'] = this.reader.getFloat(translation,'y',true);
+	this.initialsInfo.translation['z'] = this.reader.getFloat(translation,'z',true);
+
+	if(isNaN(this.initialsInfo.translation['x'])) return "'translation x' must be a float.";
+	if(isNaN(this.initialsInfo.translation['y'])) return "'translation y' must be a float.";
+	if(isNaN(this.initialsInfo.translation['z'])) return "'translation z' must be a float.";
+
+	//Rotation
+	elemsRotation = initials.getElementsByTagName('rotation');
+	if(elemsRotation == null) return "'rotation' tag is missing.";
+	if(elemsRotation.length != 3) return "'rotation' tag: exactly 3 required.";
+	
+	this.initialsInfo.rotation={};
+
+	this.initialsInfo.rotation[this.reader.getString(elemsRotation[0],'axis',true)] = this.reader.getFloat(elemsRotation[0],'angle',true);
+	this.initialsInfo.rotation[this.reader.getString(elemsRotation[1],'axis',true)] = this.reader.getFloat(elemsRotation[1],'angle',true);
+	this.initialsInfo.rotation[this.reader.getString(elemsRotation[2],'axis',true)] = this.reader.getFloat(elemsRotation[2],'angle',true);
+
+	if(this.reader.getString(elemsRotation[0],'axis',true) != "x")  return "value of the first axis be x.";
+	if(this.reader.getString(elemsRotation[1],'axis',true) != "y")	return "value of the second must be y.";
+	if(this.reader.getString(elemsRotation[2],'axis',true) != "z")	return "value of the third must be z.";
+
+	if(isNaN(this.initialsInfo.rotation[this.reader.getString(elemsRotation[0],'axis',true)])) return "'angle' in x axis must be a float.";
+	if(isNaN(this.initialsInfo.rotation[this.reader.getString(elemsRotation[1],'axis',true)])) return "'angle' in y axis must be a float.";
+	if(isNaN(this.initialsInfo.rotation[this.reader.getString(elemsRotation[2],'axis',true)])) return "'angle' in z axis must be a float.";
+
+	//Scale
+	elemsScale = initials.getElementsByTagName('scale');
+	if(elemsScale == null) return "'scale' tag is missing.";
+	if(elemsScale.length != 1) return "'More than one 'scale' tag.";
+	var scale = elemsScale[0];
+
+	this.initialsInfo.scale={};
+
+	this.initialsInfo.scale['sx'] = this.reader.getFloat(scale,'sx',true);
+	this.initialsInfo.scale['sy'] = this.reader.getFloat(scale,'sy',true);
+	this.initialsInfo.scale['sz'] = this.reader.getFloat(scale,'sz',true);
+
+	if(isNaN(this.initialsInfo.scale['sx'])) return "'scale sx' must be a float.";
+	if(isNaN(this.initialsInfo.scale['sy'])) return "'scale sy' must be a float";
+	if(isNaN(this.initialsInfo.scale['sz'])) return "'scale sz' must be a float";
+
+    //Reference
+    elemsReference = initials.getElementsByTagName('reference');
+	if(elemsFrustum == null) return "'reference' tag is missing.";
+	if(elemsFrustum.length != 1) return "More than one 'reference' tag.";
+	var reference = elemsReference[0];
+
+	this.initialsInfo.reference={};
+
+	this.initialsInfo.reference['length'] = this.reader.getFloat(reference,'length',true);
+
+	if(isNaN(this.initialsInfo.reference['length'])) return "'reference length' must be a float.";
 
 }
 
