d5b32ce68b929d8cdc104b7d4891f6e622a52a8b
joaorpsoares <joao_rp_soares@hotmail.com>
2015-10-19 14:08:42 +0100

    nodes

--- reader/XMLscene.js
+++ reader/XMLscene.js
@@ -22,25 +22,26 @@ XMLscene.prototype.init = function (application) {
     this.leaveslist = [];
     this.texturesList = [];
     this.materialsList = [];
+    this.nodesList = [];
 
 	this.axis=new CGFaxis(this);
 
 	//this.wall = new Plane(this, [0,5,5,0]);
 
 	// Material Wall
-	this.materialWall = new CGFappearance(this);
+/*	this.materialWall = new CGFappearance(this);
 	this.materialWall.setAmbient(0.5, 0.5, 0.5, 1);
 	this.materialWall.setDiffuse(0.5, 0.3, 0.5, 1);
 	this.materialWall.setSpecular(0.15, 0.15, 0.15, 1);
 	this.materialWall.setShininess(10);
 	this.materialWall.loadTexture("floor.png");
-	this.materialWall.setTextureWrap("CLAMP_TO_EDGE", "CLAMP_TO_EDGE");
+	this.materialWall.setTextureWrap("CLAMP_TO_EDGE", "CLAMP_TO_EDGE");*/
 
 
 	//this.cylinder = new MyCoveredCylinder(this,[20,6,3,20,20]);
 
 	// Material Wall
-	this.materialcylinder = new CGFappearance(this);
+	/*this.materialcylinder = new CGFappearance(this);
 	this.materialcylinder.setAmbient(0.5, 0.5, 0.5, 1);
 	this.materialcylinder.setDiffuse(0.5, 0.3, 0.5, 1);
 	this.materialcylinder.setSpecular(0.15, 0.15, 0.15, 1);
@@ -57,7 +58,7 @@ XMLscene.prototype.init = function (application) {
 	this.materialtriangle.setSpecular(0.15, 0.15, 0.15, 1);
 	this.materialtriangle.setShininess(10);
 	this.materialtriangle.loadTexture("head.png");
-	this.materialtriangle.setTextureWrap("CLAMP_TO_EDGE", "CLAMP_TO_EDGE");
+	this.materialtriangle.setTextureWrap("CLAMP_TO_EDGE", "CLAMP_TO_EDGE");*/
 
 }
 
@@ -84,7 +85,6 @@ XMLscene.prototype.initLights = function () {
 	}
 
     this.shader.unbind();
-    this.shader.unbind();
 };
 
 XMLscene.prototype.initCameras = function () {
@@ -92,9 +92,12 @@ XMLscene.prototype.initCameras = function () {
 };
 
 XMLscene.prototype.setDefaultAppearance = function () {
-    /*this.setDiffuse(0.2, 0.4, 0.8, 1.0);
-    this.setSpecular(0.2, 0.4, 0.8, 1.0);
-    this.setShininess(10.0);	*/
+    for (var i = 0; i < this.materialsList.length; i++) {
+        if (this.materialsList[i].id == "default") {
+            this.materialsList[i].apply();
+            break;
+        }
+    }
 };
 
 // Handler called when the graph is finally loaded. 
@@ -149,6 +152,10 @@ XMLscene.prototype.onGraphLoaded = function ()
 
     //LEAVES
     this.setLeaves();
+
+
+    //NODES
+    this.setNodes();
 };
 
 XMLscene.prototype.display = function () {
@@ -185,9 +192,26 @@ XMLscene.prototype.display = function () {
         // setInitials transformations
         this.setInitials();
 
-				//Lights
+		//Lights
         for (var i = 0; i < this.graph.lightsList.length; i++)
             this.lights[i].update();
+
+        //Nodes
+
+        for(var i = 0; i < this.nodesList.length; i++){
+        	console.log(this.nodesList[i]);
+        /*
+        	var node = this.nodesList[i];
+            this.pushMatrix();
+            node["material"].setTexture(node["texture"]);
+            if (node["texture"] != null) {
+                node["primitive"].updateTex(node["texture"].amplifFactor_S, node["texture"].amplifFactor_T);
+            }
+            node["material"].apply();
+            this.multMatrix(node["matrix"]);
+            node["primitive"].display();
+            this.popMatrix();*/
+        }
 	}
 
 	    // Plane Wall
@@ -204,7 +228,7 @@ XMLscene.prototype.display = function () {
 	//**************************************************************
 	//TESTES ÀS LEAFS INTRODUZIdas
 	//retangulo (primeira leaf logo posiçao zero no vector de leafs)
-	this.materialWall.apply();
+	/*this.materialWall.apply();
 	this.leaveslist[0].display();
 	//triangulo
 	this.materialtriangle.apply();
@@ -221,7 +245,7 @@ XMLscene.prototype.display = function () {
 	this.translate(0, 0, 1);
 	this.materialtriangle.apply();
 	this.triangle.display();
-	this.popMatrix();
+	this.popMatrix();*/
 
     this.shader.unbind();
 };
@@ -273,4 +297,93 @@ XMLscene.prototype.setLeaves = function() {
                 break;
         }
 	}
-}
+};
+
+XMLscene.prototype.findNode = function(id) {
+	
+	for(var i = 0; i<this.graph.nodesList.length; i++){
+		if(this.graph.nodesList[i].id == id){
+			return this.graph.nodesList[i];
+		}
+
+	}
+
+};
+
+XMLscene.prototype.setNodes = function() {
+
+	var root = this.findNode(this.graph.rootInfo["id"]);
+    this.calcNodes(root, root.texture, root.material, root.matrix);
+};
+
+XMLscene.prototype.calcNodes = function(node, nodeTexture, nodeMaterial, nodeMatrix) {
+
+	if(nodeTexture == "null")
+		var nextNodeTexture = nodeTexture;
+	else{
+		this.nextNodeTexture = node.texture;
+	}
+
+	if(nodeMaterial == "null")
+		var nextNodeMaterial = nodeMaterial;
+	else{
+		this.nextNodeMaterial = node.material;
+	}
+		
+	var nextNodeMatrix = mat4.create();
+	mat4.multiply(nextNodeMatrix, nodeMatrix, node.matrix);
+
+	for(var i = 0; i < node.descendants.length; i++){
+		
+		var nextNode = this.findNode(node.descendants[i]);
+
+		if(nextNode == null){
+
+			this.n = [];
+			this.n["id"] = node.descendants[i];
+
+			//textures
+			if(nextNodeTexture == null)
+				this.n["texture"] = null;
+			else{
+				for(var j = 0; j<this.texturesList.length; j++){
+
+					if(nextNodeTexture == this.texturesList[j]["id"]){
+						this.n["texture"] = this.texturesList[j];
+					}
+				}
+			}
+
+			//materials
+
+			if(nextNodeMaterial == null)
+				this.n["material"] = null;
+
+			else{
+				for(var k = 0; k<this.materialsList.length; k++){
+
+					if(nextNodeMaterial == this.materialsList[k]["id"])
+						this.n["material"] = this.materialsList[k];
+				}
+			}
+
+			this.n["matrix"] = nextNodeMatrix;
+
+			for(var l = 0; l < this.leaveslist.length; l++){
+
+				if(this.leaveslist[l].id == this.n["id"]){
+					this.n["primitive"] = this.leaveslist[l];
+					break;
+				}
+			}
+
+			this.nodesList.push(this.n);
+			continue;
+
+		}
+
+		this.calcNodes(nextNode, nextNodeTexture, nextNodeMaterial, nextNodeMatrix);
+
+	}
+
+};
