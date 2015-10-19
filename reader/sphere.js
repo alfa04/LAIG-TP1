0dc908a98eca2daf503e124683f4db6779ff376e
alfa04 <joao.carlos.costa@fe.up.pt>
2015-10-19 23:30:27 +0100

    esfera alterada

--- reader/sphere.js
+++ reader/sphere.js
@@ -1,15 +1,10 @@
-/**
- * sphere
- * @constructor
- */
- function sphere(scene, args, s, t) {
+	function sphere(scene, args, s, t) {
  	CGFobject.call(this,scene);
 	
 	this.radius = args[0];
 	this.slices = args[1];
 	this.stacks = args[2];
 	
-
 	this.s = s || 1;
 	this.t = t || 1;
 
@@ -28,55 +23,44 @@
 
 	var ang_0 = 2 * Math.PI / this.slices;
 	var ang_1 = Math.PI / this.stacks;
-
-	var ang_1_now = 0;
-	var ang_1_then = ang_1;
+	var angle_fist = 0;
+	var angle_second = ang_1;
 	var ind_j = 0;
 	var aux_j = 4 * this.slices;
-
-	var tex_height = Math.PI * this.radius / this.t;
-	var tex_length = 2 * Math.PI * this.radius / this.s;
+	var texture_h = Math.PI * this.radius / this.t;
+	var texture_l = 2 * Math.PI * this.radius / this.s;
 	
 	for (j = 0; j < this.stacks; j++) {
 		
-		var ang_0_now = 0;
+		var angle_fist0 = 0;
 		var ind_i = 0;
 
 		for (i = 0; i < this.slices; i++) {
+			var x0 = this.radius * Math.sin(angle_fist) * Math.cos(angle_fist0);
+			var y0 = this.radius * Math.cos(angle_fist);
+			var z0 = this.radius * Math.sin(angle_fist) * Math.sin(angle_fist0);
 
-			var x0 = this.radius * Math.sin(ang_1_now) * Math.cos(ang_0_now);
-			var y0 = this.radius * Math.cos(ang_1_now);
-			var z0 = this.radius * Math.sin(ang_1_now) * Math.sin(ang_0_now);
+			var x2 = this.radius * Math.sin(angle_second) * Math.cos(angle_fist0);
+			var y2 = this.radius * Math.cos(angle_second);
+			var z2 = this.radius * Math.sin(angle_second) * Math.sin(angle_fist0);
 
-			var x2 = this.radius * Math.sin(ang_1_then) * Math.cos(ang_0_now);
-			var y2 = this.radius * Math.cos(ang_1_then);
-			var z2 = this.radius * Math.sin(ang_1_then) * Math.sin(ang_0_now);
+			angle_fist0 += ang_0;
 
-			ang_0_now += ang_0;
+			var x1 = this.radius * Math.sin(angle_fist) * Math.cos(angle_fist0);
+			var y1 = this.radius * Math.cos(angle_fist);
+			var z1 = this.radius * Math.sin(angle_fist) * Math.sin(angle_fist0);
 
-			var x1 = this.radius * Math.sin(ang_1_now) * Math.cos(ang_0_now);
-			var y1 = this.radius * Math.cos(ang_1_now);
-			var z1 = this.radius * Math.sin(ang_1_now) * Math.sin(ang_0_now);
+			var x3 = this.radius * Math.sin(angle_second) * Math.cos(angle_fist0);
+			var y3 = this.radius * Math.cos(angle_second);
+			var z3 = this.radius * Math.sin(angle_second) * Math.sin(angle_fist0);
 
-			var x3 = this.radius * Math.sin(ang_1_then) * Math.cos(ang_0_now);
-			var y3 = this.radius * Math.cos(ang_1_then);
-			var z3 = this.radius * Math.sin(ang_1_then) * Math.sin(ang_0_now);
+			this.vertices.push(x0,y0,z0); // vertice 0
 
-			this.vertices.push(x0);
-			this.vertices.push(y0);
-			this.vertices.push(z0); // vertice 0
+			this.vertices.push(x1,y1,z1); // vertice 1
 
-			this.vertices.push(x1);
-			this.vertices.push(y1);
-			this.vertices.push(z1); // vertice 1
+			this.vertices.push(x2,y2,z2) // vertice 2
 
-			this.vertices.push(x2)
-			this.vertices.push(y2);
-			this.vertices.push(z2); // vertice 2
-
-			this.vertices.push(x3);
-			this.vertices.push(y3);
- 			this.vertices.push(z3); // vertice 3
+			this.vertices.push(x3,y3,z3); // vertice 3
 
  			var ind_i_j = ind_i + ind_j;
 
@@ -90,45 +74,29 @@
 
 			ind_i += 4;
 
-			// normal a vertice 0
-			this.normals.push(x0);
-			this.normals.push(y0);
-			this.normals.push(z0);
+			// vertice 0
+			this.normals.push(x0,y0,z0);
 			
-			// normal a vertice 1
-            this.normals.push(x1);
-			this.normals.push(y1);
-			this.normals.push(z1);
+			// vertice 1
+            this.normals.push(x1,y1,z1);
 
-			// normal a vertice 2
-			this.normals.push(x2);
-			this.normals.push(y2);
-			this.normals.push(z2);
+			// vertice 2
+			this.normals.push(x2,y2,z2);
 			
-			// normal a vertice 3
-            this.normals.push(x3);
-			this.normals.push(y3);
-			this.normals.push(z3);
+			// vertice 3
+            this.normals.push(x3,y3,z3);
 
-			// coordenadas textura
-/*			this.texCoords.push(tex_length * (1 - i / this.slices), tex_height * j / this.stacks);
-			this.texCoords.push(tex_length * (1 - (i + 1) / this.slices), tex_height * j / this.stacks);
-			this.texCoords.push(tex_length * (1 - i / this.slices), tex_height * (j + 1) / this.stacks);
-			this.texCoords.push(tex_length * (1 - (i + 1) / this.slices), tex_height * (j + 1) / this.stacks);*/
-
+			// coordenadas texturas
 			this.texCoords.push(1 - i / this.slices, j / this.stacks);
 			this.texCoords.push(1 - (i + 1) / this.slices, j / this.stacks);
 			this.texCoords.push(1 - i / this.slices, (j + 1) / this.stacks);
 			this.texCoords.push(1 - (i + 1) / this.slices, (j + 1) / this.stacks);
 
-		}
-					
-		ang_1_now += ang_1;
-		ang_1_then += ang_1;
+		}			
+		angle_fist += ang_1;
+		angle_second += ang_1;
 		ind_j += aux_j;
-
 	}
-
  	this.primitiveType = this.scene.gl.TRIANGLES;
  	this.initGLBuffers();
  	
